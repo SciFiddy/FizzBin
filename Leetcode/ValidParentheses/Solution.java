eff6d24045bb6cf57f4511b1b698dec8da31df00
Brian Abrahamsen <brian.abrahamsen@gmail.com>
2022-10-02 12:47:01 -0700

    Valid Parentheses guess one
    Valid Parentheses guess one

--- Leetcode/ValidParentheses/Solution.java
+++ Leetcode/ValidParentheses/Solution.java
@@ -1,17 +1,70 @@
-class Solution {
+import java.util.Map;
+import java.util.Stack;
 
+/*
+
+                                        https://leetcode.com/problems/valid-parentheses/
+
+        Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine
+        if the input string is valid.
+
+        An input string is valid if:
+
+            Open brackets must be closed by the same type of brackets.
+            Open brackets must be closed in the correct order.
+            Every close bracket has a corresponding open bracket of the same type.
+
+        Example 1:
+
+        Input: s = "()"
+        Output: true
+
+        Example 2:
+
+        Input: s = "()[]{}"
+        Output: true
+
+        Example 3:
+
+        Input: s = "(]"
+        Output: false
+
+        Constraints:
+
+            1 <= s.length <= 104
+            s consists of parentheses only '()[]{}'
+
+*/
+
+class Solution {
     public static void main(String[] args) {
-        System.out.println(isValid("[]"));
+        System.out.println(isValid("([()][][]{}{}()())"));
     }
 
-
-    public boolean isValid(String s) {
-        HashMap<char, char> yrmp = Map.of(
-                '(', ')', '{', '}', '[', ']'
+    public static boolean isValid(String s) {
+        boolean result = true;
+        Map<Character, Character> yrmp = Map.of(
+                ')', '(', '}', '{', ']', '['
         );
-
         System.out.println(yrmp);
-
-        return false;
+        Stack<Character> stacko = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case '(':
+                case '[':
+                case '{':
+                    stacko.push(s.charAt(i));
+                    break;
+                case ')':
+                case ']':
+                case '}':
+                    if (!stacko.pop().equals(yrmp.get(s.charAt(i))))
+                        result = false;
+                    break;
+                default:
+                    System.out.println("Harry Kim was here");
+            }
+        }
+        return result;
     }
 }
